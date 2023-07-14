4c5b9f38048d9b3f16f34e837c1528262058e50b
Luca <lucacarena98@gmail.com>
2023-07-14 19:40:36 -0300

    padding

--- src/components/bebidas/Cervezas.jsx
+++ src/components/bebidas/Cervezas.jsx
@@ -25,10 +25,8 @@ export function Cervezas() {
           <tbody>
             {cervezas.map((item) => (
               <tr key={item.id}>
-                <td className="w-3/4 px-5 text-base">{item.producto}</td>
-                <td className="w-1/4 px-5 text-right text-base">
-                  ${item.precio}
-                </td>
+                <td className="w-3/4 text-base">{item.producto}</td>
+                <td className="w-1/4 text-right text-base">${item.precio}</td>
               </tr>
             ))}
           </tbody>
