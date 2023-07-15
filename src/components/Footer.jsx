45b576533501fbeadc1af785641160d239663d60
Luca <lucacarena98@gmail.com>
2023-07-15 20:48:38 -0300

    colores4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="text-center  p-4">
-      <section className="border-2 bg-slate-400 p-5 boton-color">
+      <section className="border-2 bg-slate-400 p-3 boton-color">
         <p className="font-quicksand font-semibold ">Ubicación</p>
         <p>123 Calle Principal, Alcira</p>
         <p>Código Postal 5813</p>
