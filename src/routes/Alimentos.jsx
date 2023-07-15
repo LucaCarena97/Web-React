217654d67ca028ecad689656213d36ff7f4998c8
Luca <lucacarena98@gmail.com>
2023-07-15 11:47:24 -0300

    boton

--- src/routes/Alimentos.jsx
+++ src/routes/Alimentos.jsx
@@ -7,97 +7,101 @@ export function Alimentos() {
       <section className="ml-5">
         <Menu />
       </section>
-      <section className="flex flex-col items-center m-5 ">
+      <section className="flex flex-col items-center m-6 gap-2">
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 w-48 rounded hover:bg-yellow-200 mb-2 text-center"
           to={"/alimentos/aceite"}
         >
           Aceite
         </Link>
         <Link
-          className="bg-red-100 border-yellow-500 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-red-200 mb-2 text-center"
+          className="bg-red-100 border-yellow-500 border-4 text-lg py-2 w-48 rounded hover:bg-red-200 mb-2 text-center"
           to={"/alimentos/azucar"}
         >
           Azúcar
         </Link>
         <Link
-          className="bg-yellow-800 border-yellow-500 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-700 mb-2 text-center"
+          className="bg-yellow-700 border-yellow-500 border-4 text-lg py-2 w-48 rounded hover:bg-yellow-600 mb-2 text-center"
           to={"/alimentos/cafe"}
         >
           Café
         </Link>
         <Link
-          className="bg-yellow-100 border-yellow-300 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-yellow-100 border-yellow-300 border-4 text-lg py-2 w-48 rounded hover:bg-yellow-200 mb-2 text-center"
           to={"/alimentos/durazno"}
         >
           Durazno
         </Link>
         <Link
-          className="bg-red-400 border-red-500 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-red-300 mb-2 text-center"
+          className="bg-red-400 border-red-500 border-4 text-lg py-2 w-48 rounded hover:bg-red-300 mb-2 text-center"
           to={"/alimentos/firenze"}
         >
           Firenze
         </Link>
+
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-red-300 border-green-300 border-4 text-lg py-2 w-48 rounded hover:bg-red-400 mb-2 text-center"
           to={"/alimentos/gallo"}
         >
           Gallo
         </Link>
+
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-blue-400 border-blue-500 border-4 text-lg py-2 w-48 rounded hover:bg-blue-300 mb-2 text-center"
           to={"/alimentos/golosinas"}
         >
           Golosinas
         </Link>
+
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-gray-300 border-gray-400 border-4 text-lg py-2 w-48 rounded hover:bg-gray-200 mb-2 text-center"
           to={"/alimentos/harina"}
         >
           Harina
         </Link>
+
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-green-300 border-yellow-300 border-4 text-lg py-2 w-48 rounded hover:bg-green-400 mb-2 text-center"
           to={"/alimentos/jugo-limon"}
         >
           Jugo Limón
         </Link>
 
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-pink-50 border-blue-200 border-4 text-lg py-2 w-48 rounded hover:bg-pink-100 mb-2 text-center"
           to={"/alimentos/leche"}
         >
           Leche
         </Link>
 
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-green-200 border-green-300 border-4 text-lg py-2 w-48 rounded hover:bg-green-100 mb-2 text-center"
           to={"/alimentos/mate-cocido"}
         >
           Mate Cocido/Té
         </Link>
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-pink-300 border-pink-400 border-4 text-lg py-2 w-48 rounded hover:bg-pink-200 mb-2 text-center"
           to={"/alimentos/pulpa"}
         >
           Pulpa Cocktelera
         </Link>
 
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-yellow-500 border-yellow-600 border-4 text-lg py-2 w-48 rounded hover:bg-yellow-400 mb-2 text-center"
           to={"/alimentos/quento"}
         >
           Quento
         </Link>
 
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-red-300 border-red-400 border-4 text-lg py-2 w-48 rounded hover:bg-red-200 mb-2 text-center"
           to={"/alimentos/triturado"}
         >
           Tomate triturado
         </Link>
         <Link
-          className="bg-yellow-300 border-yellow-400 border-4 text-lg py-2 px-3 w-36 rounded hover:bg-yellow-200 mb-2 text-center"
+          className="bg-green-300 border-green-400 border-4 text-lg py-2 w-48 rounded hover:bg-green-200 mb-2 text-center"
           to={"/alimentos/yerba"}
         >
           Yerba
