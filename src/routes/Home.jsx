c91d516886eaa9a0f1bc0a05744f1909592d0c89
Luca <lucacarena98@gmail.com>
2023-07-16 18:55:53 -0300

    iconos

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,34 +1,39 @@
 import { Link } from "react-router-dom";
+import { BiSolidDrink } from "react-icons/bi";
+import { BiSolidCookie } from "react-icons/bi";
+import { MdOutlineCleanHands } from "react-icons/md";
+import { BiSolidCategoryAlt } from "react-icons/bi";
 
 export function Home() {
   return (
     <main className="font-quicksand font-semibold text-center text-lg custom-height-80vh">
       <section className="flex flex-col gap-7 items-center p-7">
         <Link
-          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600 flex items-center justify-center"
           to={"/alimentos"}
         >
-          Alimentos
+          <BiSolidCookie className="mr-2" /> Alimentos
         </Link>
 
         <Link
-          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600 flex items-center justify-center"
           to={"/bebidas"}
         >
-          Bebidas
+          <BiSolidDrink className="mr-2" /> Bebidas
         </Link>
 
         <Link
-          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600 flex items-center justify-center"
           to={"/higiene"}
         >
-          Higiene
+          <MdOutlineCleanHands className="mr-2" /> Higiene
         </Link>
+
         <Link
-          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600"
+          className="bg-blue-500 w-40 border-blue-600 border-2 text-white py-3 px-4 rounded hover:bg-blue-600 flex items-center justify-center"
           to={"/varios"}
         >
-          Otros
+          <BiSolidCategoryAlt className="mr-2" /> Otros
         </Link>
       </section>
     </main>
