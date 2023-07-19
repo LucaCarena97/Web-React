4b1eabdc888fe27ff57fb7a4d8587d3e9628640d
Luca <lucacarena98@gmail.com>
2023-07-19 19:13:02 -0300

    animated

--- src/components/bebidas/Aperitivos.jsx
+++ src/components/bebidas/Aperitivos.jsx
@@ -35,7 +35,7 @@ export function Aperitivos() {
         </div>
       </section>
 
-      <section className="flex justify-center select-none animate__animated animate__flipInX">
+      <section className="flex justify-center select-none animate__animated animate__zoomIn">
         <table className="table-fixed mt-5 border-2 border-blue-300">
           <thead>
             <tr className="border-2 border-blue-400 text-lg bg-blue-300 boton-color">
