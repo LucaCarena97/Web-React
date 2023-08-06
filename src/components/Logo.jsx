7baea08d1821d1233bd926170a87fe546e8b01ca
Luca Carena <lucacarena98@gmail.com>
2023-08-06 19:13:15 -0300

    shadow

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <section
       className="select-none font-quicksand font-semibold text-base
-      border-blue-600 border-2 p-2  animate__animated animate__backInDown logo"
+      border-blue-600 border-2 p-2 logo"
     >
       <p>Le</p>
       <p>Nostre</p>
